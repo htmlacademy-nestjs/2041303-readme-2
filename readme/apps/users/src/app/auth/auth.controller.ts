6eafa165bd12dff6189d4f99234f084496e5e198
temnigor <temnigor@gmail.com>
2022-11-27 18:06:38 +0300

    abb OpenApi

--- readme/apps/users/src/app/auth/auth.controller.ts
+++ readme/apps/users/src/app/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/creat-user.dto';
 import {getDataToDto} from 'libs/core/src/index';
 import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
 
@@ -14,18 +16,32 @@ export class AuthController {
   ) {}
 
 @Post('register')
+@ApiResponse({ status: HttpStatus.CREATED, description: 'The user has been successfully created.'})
+@ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.'})
 async create(@Body() dto:CreateUserDto) {
 const newUser = await this.authService.register(dto);
 return getDataToDto(UserRdo, newUser);
 }
 
 @Post('login')
+@ApiResponse({
+  type: LoggedUserRdo,
+  status: HttpStatus.OK,
+  description: 'The user successfully logged.'
+})
+@ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Password or login wrong'})
 async login(@Body() dto:LoginUserDto) {
   const verifyUser = this.authService.verifyUser(dto);
   return getDataToDto(LoggedUserRdo, verifyUser);
 }
 
 @Get(':id')
+@ApiResponse({
+  type:UserRdo,
+  status: HttpStatus.OK,
+  description: 'The user exist.'
+  })
+@ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.'})
 async show(@Param('id') id:string){
   const existsUser = await this.authService.getUserId(id);
   return getDataToDto(UserRdo, existsUser);
--- readme/apps/users/src/app/auth/dto/login-user.dto.ts
+++ readme/apps/users/src/app/auth/dto/login-user.dto.ts
@@ -1,6 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class LoginUserDto {
 
+@ApiProperty({
+  description: 'User uniq email',
+  example: 'user@user.ru',
+})
 public  email:string;
+
+@ApiProperty({
+  description: 'User uniq password',
+  example: '1357dfG',
+})
 public  password:string;
 
 }
